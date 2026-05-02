fe47c28b11eb9acea04422de53f20e8c7d32e3cb
MirajMalik <mirajmalik333@gmail.com>
2026-05-02 21:53:14 +0600

    Initial Commit - Dream Team

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+// import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+
+  return (
+    <button className="btn m-10 bg-red-900 p-3 cursor pointer">Default</button>
+  )
+}
+
+export default App
