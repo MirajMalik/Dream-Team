870a84d02f44463f980cfe421f254abc0724d9db
MirajMalik <mirajmalik333@gmail.com>
2026-05-03 13:07:01 +0600

    created dummy players data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './index.css'
-// import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
 
   return (
-    <button className="btn m-10 bg-red-900 p-3 cursor pointer">Default</button>
+    <div>
+         <Navbar />
+    </div>
+    
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,17 @@
-function Navbar() {
-  return (
-    <div className="navbar bg-base-100 shadow-sm">
-        <div className="flex-1">
-            <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="flex gap-2">
-            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-            <div className="dropdown dropdown-end">
-            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                <div className="w-10 rounded-full">
-                    <img
-                        alt="Tailwind CSS Navbar component"
-                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" 
-                    />
-                </div>
-        </div>
+import logo from '/src/assets/logo.png'
+import coinImage from '/src/assets/dollar.png'
 
-        <ul
-            tabIndex="-1"
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-            <li>
-                <a className="justify-between">
-                    Profile
-                    <span className="badge">New</span>
-                </a>
-            </li>
-            <li><a>Settings</a></li>
-            <li><a>Logout</a></li>
-        </ul>
+function Navbar() {
+return (
+    <div className="navbar max-w-[1200px] mx-auto bg-base-300 shadow-sm">
+        <div className="flex-1">
+            <img className="w-[60px] h-[60px] ml-4" src={logo} alt="Logo" />
         </div>
-    </div>
+        <div className="flex items-center gap-1 mr-4 font-bold">
+            <span>6000000</span>
+            <span>Coin</span>
+            <img className='w-[20px] h-[20px]' src={coinImage} alt="coin image" />
+        </div>
     </div>
     )
 }
